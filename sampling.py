7032b2c25126c41ce933a677e0aeeabf16aeca4e
Joao <joao.isler@bsc.es>
2026-03-09 16:04:16 +0100

    Add boundary condition sampling

--- sampling.py
+++ sampling.py
@@ -5,13 +5,16 @@ import re
 from pathlib import Path
 import plot as pl
 
-
 class SamplingData:
     # Initialize the class
     def __init__(self, params):
 
         # Dimension
         self.dims = params['dims']
+        self.pts_in = np.empty((0, 3), dtype=float)
+        self.pts_bc = np.empty((0, 3), dtype=float)
+        self.pts_grad = np.empty((0, 3), dtype=float)
+        self.pts = np.empty((0, 3), dtype=float)
 
         # Load your solution
         # .vtk, .pvtu, .vtm, ...
@@ -27,13 +30,20 @@ class SamplingData:
 
         if params['sampling']['nspoin'] > 0:
             # Call chosen sampler 
-            pts = base_sampler(params['sampling']['nspoin'], xmin, xmax, ymin, ymax)  
+            pts_in = base_sampler(params['sampling']['nspoin'], xmin, xmax, ymin, ymax)  
             # The flow is 2D but VTK expects 3D points, lift to z=zmin (or 0)
             if self.dims == 2:
-                pts = np.column_stack([pts, np.full((pts.shape[0],), zmin)])
+                pts_in = np.column_stack([pts_in, np.full((pts_in.shape[0],), zmin)])
         else:
             raise ValueError("Number of sample points must be provided ")
-
+        self.pts_in = np.vstack([self.pts_in, pts_in])
+        # Apply mask at the inner points
+        sampled = pv.PolyData(self.pts_in).sample(mesh)
+        mask = sampled["vtkValidPointMask"].astype(bool)
+        self.pts_in = self.pts_in[mask]
+        # All points
+        self.pts = np.vstack([self.pts, pts_in])
+        
         # Add extra points in regions detected by a sensor
         # Extra points based on gradient |grad(rho)|
         if params['sampling']['nspoin_grad'] > 0:
@@ -45,17 +55,26 @@ class SamplingData:
                 pool_factor=params['sampling'].get('pool_factor', 8),
                 alpha=params['sampling'].get('alpha_grad_rho', 1.5),
                 seed=params['sampling'].get('seed', 1234))
-            pts = np.vstack([pts, pts_grad])
+            self.pts_grad = np.vstack([self.pts_grad, pts_grad])
+            self.pts = np.vstack([self.pts, pts_grad])
 
         # Points on the boundary condition
-        for phys_name in params['sampling']['bc']:
-            pts_bc = self.SampleBoundaryCondition(phys_name, params)
-            #pts = np.vstack([pts, pts_bc])
+        bc_names = params['sampling']['bc']
+        bc_poin = params['sampling']['nspoin_bc']
+        for phys_name, n_bc in zip(bc_names, bc_poin):
+            if n_bc > 0:
+                pts_bc = self.SampleBoundaryCondition(phys_name, n_bc, params)                
+                pts_bc = self.NudgeBCPoints(pts_bc, phys_name, xmin, xmax, ymin, ymax)
+                self.pts_bc = np.vstack([self.pts_bc, pts_bc])
+                self.pts = np.vstack([self.pts, pts_bc])
 
-        # Interpolate solution at points
-        point_cloud = pv.PolyData(pts)
+        # Interpolate solution at all points
+        point_cloud = pv.PolyData(self.pts)
         # Interpolates point/cell data onto pts
         sampled = point_cloud.sample(mesh)  
+        # Apply the mask in all points
+        mask = sampled["vtkValidPointMask"].astype(bool)
+        self.pts = self.pts[mask]
 
         # sampled.point_data now contains interpolated arrays at your points
         #print(sampled.point_data.keys())
@@ -67,19 +86,13 @@ class SamplingData:
         rho = sampled["Density"]  # (N,) 
         p = sampled["Pressure"]   # (N,) if scalar
 
-        # Get points inside the geometry
-        if "vtkValidPointMask" in sampled.point_data:
-            mask = sampled["vtkValidPointMask"].astype(bool)
-        else:
-            raise RuntimeError("vtkValidPointMask not found")
-
         # Remove invalid points and normalised it
-        self.Xstar = X[mask] / params['Lref']
+        self.Xstar = X[mask] / params['Lref'] 
         self.rhostar = rho[mask] / params['rho']
         self.Ustar = U[mask] / params['U_0']
         self.pstar = p[mask] / (params['rho'] 
             * params['U_0'] * params['U_0']) 
-
+        
     def GetBaseSampler(self, sampling_type: str):
         if sampling_type == "random":
             return self.SampleRandomPoints
@@ -114,7 +127,7 @@ class SamplingData:
         
         return pts
     
-    def SampleBoundaryCondition(self, phys_name, params):
+    def SampleBoundaryCondition(self, phys_name, npoin_bc, params):
         """
         Sample boundary points from a Physical Group in a .geo file.
         phys_name: physical group name in the .geo, e.g. "inlet", "outlet", 
@@ -122,6 +135,8 @@ class SamplingData:
         Returns: 
         """
         rng = np.random.default_rng(params['sampling']['seed'])
+        if npoin_bc <= 0:
+            raise ValueError("params['sampling']['nspoin_bc'] must be > 0")
 
         gmsh.initialize()
         # Turn off terminal output from Gmsh
@@ -129,7 +144,10 @@ class SamplingData:
         try:
             gmsh.open(params['pathMesh']+'/'+params['mesh'])
             gmsh.model.geo.synchronize()
-            gmsh.model.mesh.generate(self.dims)
+            #gmsh.model.mesh.generate(self.dims)
+            # For 1D mesh generation before 2D
+            gmsh.model.mesh.generate(self.dims-1)   # mesh curves (boundary)
+            gmsh.model.mesh.generate(self.dims)   # mesh surface
 
             # Find physical group tag by name
             # Gmsh stores physical groups by (dim, tag)
@@ -140,9 +158,52 @@ class SamplingData:
                 name = gmsh.model.getPhysicalName(d, tag)
                 if name == phys_name:
                     matches.append((d, tag))
-        
+
             if not matches:
                 raise ValueError(f"Physical group '{phys_name}' not found.")
+
+            # if self.dims == 2 (2D domain), boundary is dim=1 (curves)
+            # if self.dims == 3 (3D domain), boundary is dim=2 (surfaces)
+            bc_dim = self.dims - 1
+            d_use, tag_use = matches[0]  # There is only one per call
+            for d, tag in matches:
+                if d == bc_dim:
+                    d_use, tag_use = d, tag
+
+           # Robust node extraction: physical group -> entities -> nodes
+            ent_tags = gmsh.model.getEntitiesForPhysicalGroup(d_use, tag_use)
+            if len(ent_tags) == 0:
+                raise RuntimeError(f"Physical group '{phys_name}' has no entities \
+                                   (dim={d_use}, tag={tag_use}).")
+
+            all_pts_bc = []
+            for e in ent_tags:
+                nodes = gmsh.model.mesh.getNodes(d_use, e)
+                node_coords = nodes[1]  # works for both 2-return and 3-return variants
+                if len(node_coords) > 0:
+                    # Make the array (N,3)
+                    all_pts_bc.append(np.asarray(node_coords, dtype=float).reshape(-1, 3))
+
+            if not all_pts_bc:
+                raise RuntimeError(
+                    f"No nodes found for physical group '{phys_name}' on entities {ent_tags} "
+                    f"(dim={d_use}, tag={tag_use})."
+                )
+
+            # Put the list of arrays in a single array, since all_pyts is the 
+            # composition of many lists related to the possible entities that 
+            # compose the boundary condition, e.g., many lines or surfaces.
+            pts_bc = np.vstack(all_pts_bc)
+
+            # Remove duplicates (curves share endpoints)
+            # axis=0 means “consider each row [x,y,z] as one item
+            pts_bc = np.unique(pts_bc, axis=0)
+
+            # Sample with replacement if you ask for more than available.
+            # It also reduce the number of points if it is too high accordanly
+            # with the value provided (npoin_bc)
+            idx = rng.integers(0, pts_bc.shape[0], size=npoin_bc)
+            return pts_bc[idx]
 
         finally:
             gmsh.finalize()                
@@ -194,6 +255,7 @@ class SamplingData:
         else:
             pts_in = sampled.points
             grad_mag = sampled["grad_mag"]
+        
         # Check if pts_in is zero, so no point was interpolated, all points
         # were outside of the geometry.
         if pts_in.shape[0] == 0:
@@ -221,9 +283,26 @@ class SamplingData:
         else:
             pts_grad = pts_grad[:npoin_grad]
 
-        print(pts_grad)
-
         return pts_grad
+        
+    def NudgeBCPoints(self, pts_bc, name, xmin, xmax, ymin, ymax):
+        pts = pts_bc.copy()
+
+        # scale-aware eps (tiny fraction of domain size)
+        epsx = 1e-7 * (xmax - xmin)
+        epsy = 1e-7 * (ymax - ymin)
+
+        n = name.lower()
+        if n == "inlet":      # x = xmin
+            pts[:, 0] += epsx
+        elif n == "outlet":   # x = xmax
+            pts[:, 0] -= epsx
+        elif n == "bottom":   # y = ymin
+            pts[:, 1] += epsy
+        elif n == "top":      # y = ymax
+            pts[:, 1] -= epsy
+
+        return pts
     
     def GetXstar(self):       
         return self.Xstar
@@ -241,11 +320,12 @@ class SamplingData:
         out = np.column_stack([self.Xstar, self.rhostar, self.Ustar[:,0], 
             self.Ustar[:,1], self.Ustar[:,2], self.pstar])
         np.savetxt(
-            params['pathData']+'/'+params['sampling']['data_filename']+'.csv', 
+            params['pathData']+'/'+params['sampling']['fdata']+'.csv', 
             out, delimiter=",", 
             header="xstar,ystar,zstar,rhostar,ustar,vstar,wstar,pstar", 
             comments="")
 
     def PlotSamplingPointsToPDF(self, params):
-        pl.PlotSamplingPoints(self.Xstar, params)
+        pl.PlotSamplingPoints(self.Xstar, self.pts_in, self.pts_bc, \
+                              self.pts_grad, params)
 
