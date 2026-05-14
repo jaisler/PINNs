c5ea5130636c82628055b2438fa1b6593877ab28
Joāo Isler <joao.isler@gmail.com>
2026-05-14 15:12:06 +0200

    Add printing_loss method in a separate file

---
+++ src/utils/printing_loss.py
@@ -0,0 +1,67 @@
+from ..pinn.losses import loss_fn
+
+def printing_loss(pinn, it):
+
+    if pinn.model == 'pinn':
+
+        if pinn.eq == 'Euler':
+            loss_val, l_rho, l_u, l_v, l_p, l_mut, l_f1, l_f2, l_f3, l_f4 = \
+                loss_fn(pinn, return_terms=True)
+
+            print(
+                f"It: {it:6d} | "
+                f"Loss: {loss_val.item():.3e} | "
+                f"rho: {l_rho.item():.3e} | "
+                f"u: {l_u.item():.3e} | "
+                f"v: {l_v.item():.3e} | "
+                f"p: {l_p.item():.3e} | "
+                f"f1: {l_f1.item():.3e} | "
+                f"f2: {l_f2.item():.3e} | "
+                f"f3: {l_f3.item():.3e} | "
+                f"f4: {l_f4.item():.3e}"
+            )
+
+        elif pinn.eq == 'RANS':
+            loss_val, l_rho, l_u, l_v, l_p, l_mut, l_f1, l_f2, l_f3, l_f4 = \
+                loss_fn(pinn, return_terms=True)
+
+            print(
+                f"It: {it:6d} | "
+                f"Loss: {loss_val.item():.3e} | "
+                f"rho: {l_rho.item():.3e} | "
+                f"u: {l_u.item():.3e} | "
+                f"v: {l_v.item():.3e} | "
+                f"p: {l_p.item():.3e} | "
+                f"mut: {l_mut.item():.3e} | "
+                f"f1: {l_f1.item():.3e} | "
+                f"f2: {l_f2.item():.3e} | "
+                f"f3: {l_f3.item():.3e} | "
+                f"f4: {l_f4.item():.3e}"
+            )
+    
+    elif pinn.model == 'supervised':
+        loss_val, l_rho, l_u, l_v, l_p, l_mut, l_f1, l_f2, l_f3, l_f4 = \
+            loss_fn(pinn, return_terms=True)
+
+        if pinn.eq == 'Euler':
+
+            print(
+                f"It: {it:6d} | "
+                f"Loss: {loss_val.item():.3e} | "
+                f"rho: {l_rho.item():.3e} | "
+                f"u: {l_u.item():.3e} | "
+                f"v: {l_v.item():.3e} | "
+                f"p: {l_p.item():.3e} "
+            )
+
+        elif pinn.eq == 'RANS':
+
+            print(
+                f"It: {it:6d} | "
+                f"Loss: {loss_val.item():.3e} | "
+                f"rho: {l_rho.item():.3e} | "
+                f"u: {l_u.item():.3e} | "
+                f"v: {l_v.item():.3e} | "
+                f"p: {l_p.item():.3e} | "
+                f"mut: {l_mut.item():.3e}"
+            )
